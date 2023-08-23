4bfc2c4d67e9861d78c0fbd0f1168fdbbe4c7739
muhsin <muhsintt777@gmail.com>
2023-08-24 00:01:42 +0530

    feat : add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import {styles} from './HeaderStyle';
+import {Text, View} from 'react-native';
+export const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Header</Text>
+    </View>
+  );
+};
